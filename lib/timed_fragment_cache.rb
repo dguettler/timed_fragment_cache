30d7838056e8439db304159a719688471435fbd0
rlivsey <rlivsey@179e805f-c116-0410-8f81-92903350386d>
2006-08-25 05:00:25 +0000

    added initial version of timed_fragment_cache
    git-svn-id: http://svn.livsey.org/plugins/timed_fragment_cache@15 179e805f-c116-0410-8f81-92903350386d

---
+++ lib/timed_fragment_cache.rb
@@ -0,0 +1,71 @@
+# based on http://www.typosphere.org/trac/browser/trunk/vendor/plugins/expiring_action_cache/lib/metafragment.rb
+
+module ActionController
+  module Caching
+    module TimedFragment
+    
+      def self.included(base) # :nodoc:     
+        base.class_eval do 
+          alias_method :cache_erb_fragment_without_expiry, :cache_erb_fragment
+          alias_method :cache_erb_fragment, :cache_erb_fragment_with_expiry
+        end      
+      end
+      
+      def cache_erb_fragment_with_expiry(block, name = {}, options = nil, expiry = nil)
+        unless perform_caching then block.call; return end
+
+        when_fragment_expired name, expiry do
+          expire_fragment(name)
+        end      
+        
+        cache_erb_fragment_without_expiry(block, name, options)          
+      end
+    
+      def fragment_expired?(name)
+        expires = read_meta_fragment(name)
+        expires.nil? || expires < Time.now
+      end
+    
+      def read_meta_fragment(name)
+        YAML.load(read_fragment(meta_fragment_key(name))) rescue {}
+      end    
+    
+      def write_meta_fragment(name, meta)
+        write_fragment(meta_fragment_key(name), YAML.dump(meta))
+      end
+    
+      def meta_fragment_key(name)
+        fragment_cache_key(name) + '_meta'
+      end
+    
+      def when_fragment_expired(name, expiry=nil)
+        if fragment_expired? name
+          yield
+          write_meta_fragment(name, expiry)          
+        end
+      end
+    
+    end
+  end
+end
+
+module ActionView
+  module Helpers
+    module TimedFragmentCacheHelper
+    
+      def self.included(base) # :nodoc:     
+        base.class_eval do 
+          alias_method :cache, :cache_with_expiry
+        end      
+      end    
+    
+      def cache_with_expiry(name = {}, expires = nil, &block)
+        @controller.cache_erb_fragment(block, name, nil, expires)
+      end
+    
+    end
+  end
+end
+
+ActionController::Base.send :include, ActionController::Caching::TimedFragment
+ActionView::Base.send(:include, ActionView::Helpers::TimedFragmentCacheHelper)
