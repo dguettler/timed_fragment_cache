d71643be56123247afc5750b4949baa1b7172a9e
Daniel Guettler <daniel.guettler@gmail.com>
2008-07-04 12:59:35 -0400

    Rails 2.1 - update methods to work with Rails 2.1 - include timed fragment caching for ERB template handler

--- lib/timed_fragment_cache.rb
+++ lib/timed_fragment_cache.rb
@@ -7,10 +7,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -21,12 +21,27 @@
 
 module ActionController
   module Caching
-  
+
+    module TimedFragmentHandler
+
+      def self.included(base) # :nodoc:
+        base.class_eval do
+          alias_method :cache_fragment_without_expiry, :cache_fragment
+          alias_method :cache_fragment, :cache_fragment_with_expiry
+        end
+      end
+
+      def cache_fragment_with_expiry(block, name = {}, options = nil, expiry = nil) #:nodoc:
+        @view.fragment_for(block, name, options, expiry)
+      end
+
+    end
+
     # Timed Fragment Caching
     # Adds optional expiry to fragment caching which will cache that fragment for the alloted time
     # Works by adding a 'meta' fragment for each timed fragment (inspired by metafragment code in typo
     # http://www.typosphere.org/trac/browser/trunk/vendor/plugins/expiring_action_cache/lib/metafragment.rb)
-    # 
+    #
     # Works something like this:
     #
     # <% cache 'fragment_name', 10.minutes.from_now do %>
@@ -38,73 +53,67 @@ module ActionController
     # Also adds when_fragment_expired which you can use to only execute code if the fragment is not cached, or has
     # expired:
     #
-    # when_fragment_expired 'fragment_name', 10.minutes_from_now do 
+    # when_fragment_expired 'fragment_name', 10.minutes_from_now do
     #  # some intensive code
     # end
     #
     # Note that if using the 'when_fragment_expired' in the controller, you don't need the expiry in the call to cache
     # in the template, as 'when_fragment_expired' will expire the fragment for you.
     module TimedFragment
-    
-      def cache_erb_fragment(block, name = {}, options = nil, expiry = nil)
+
+      def self.included(base) # :nodoc:
+        base.class_eval do
+          alias_method :fragment_for_without_expiry, :fragment_for
+          alias_method :fragment_for, :fragment_for_with_expiry
+        end
+      end
+
+      def fragment_for_with_expiry(block, name = {}, options = nil, expiry = nil)
         unless perform_caching then block.call; return end
 
-        fragment = get_fragment(name)
-        if expiry && !fragment
-          expire_and_write_meta(name, expiry)  
+        if expiry && fragment_expired?(name)
+          expire_and_write_meta(name, expiry)
         end
 
-        buffer = eval("_erbout", block.binding)
-
-        if fragment
-          buffer.concat(fragment)
-        else
-          pos = buffer.length
-          block.call
-          write_fragment(name, buffer[pos..-1], options)
+        fragment_for_without_expiry(block, name, options) do
+          eval(ActionView::Base.erb_variable, block.binding)
         end
-  
       end
-    
+
       def expiry_time(name)
         read_meta_fragment(name)
       end
-    
-      def get_fragment(name)
-      	return fragments[name] if fragments[name]
-      	fragment = read_fragment(name)
-        return nil unless fragment
-        fragments[name] = fragment
+
+      def fragment_expired?(name)
+        return true unless read_fragment(name)
         expires = expiry_time(name)
-        return expires && expires > Time.now ? fragment : nil
+        expires.nil? || expires < Time.now
       end
 
       def read_meta_fragment(name)
         YAML.load(read_fragment(meta_fragment_key(name))) rescue nil
-      end    
-    
+      end
+
       def write_meta_fragment(name, meta)
         write_fragment(meta_fragment_key(name), YAML.dump(meta))
       end
-    
+
       def meta_fragment_key(name)
         fragment_cache_key(name) + '_meta'
       end
-    
+
       def when_fragment_expired(name, expiry=nil)
-      	return if get_fragment( name )
+        return unless fragment_expired? name
+
         yield
         expire_and_write_meta(name, expiry)
       end
-    
+
       def expire_and_write_meta(name, expiry)
         expire_fragment(name)
         write_meta_fragment(name, expiry) if expiry
       end
-    
-      def fragments
-      	@fragments ||= {}
-      end
+
     end
   end
 end
@@ -112,20 +121,37 @@ end
 module ActionView
   module Helpers
     module TimedFragmentCacheHelper
-    
-      def self.included(base) # :nodoc:     
-        base.class_eval do 
+
+      def self.included(base) # :nodoc:
+        base.class_eval do
           alias_method :cache, :cache_with_expiry
-        end      
-      end    
-    
-      def cache_with_expiry(name = {}, expires = nil, &block)
-        @controller.cache_erb_fragment(block, name, nil, expires)
+        end
       end
-    
+
+      def cache_with_expiry(name = {}, expires = nil, &block)
+        handler = Template.handler_class_for_extension(current_render_extension.to_sym)
+        handler.new(@controller).cache_fragment(block, name, nil, expires)
+      end
+
+      # Added by Daniel Guettler
+      def capture_with_cache(name, options = nil, expiry = nil, &block)
+        return capture(&block) unless @controller.perform_caching
+
+        if expiry && @controller.fragment_expired?(name)
+          @controller.expire_and_write_meta(name, expiry)
+        end
+
+        if cache = @controller.read_fragment(name, options)
+          cache
+        else
+          @controller.write_fragment(name, capture(&block), options)
+        end
+      end
+
     end
   end
 end
 
 ActionController::Base.send :include, ActionController::Caching::TimedFragment
+ActionView::TemplateHandlers::ERB.send :include, ActionController::Caching::TimedFragmentHandler
 ActionView::Base.send(:include, ActionView::Helpers::TimedFragmentCacheHelper)
